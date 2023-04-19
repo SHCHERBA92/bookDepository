cbc21b247fc3e01ec66c3ba32bf9ec71e1b438fb
SHCHERBA92 <vony4ka666@rambler.ru>
2023-04-19 16:53:23 +0300

    first commit

---
+++ src/test/java/com/test/exercise/bookdepository/BookDepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.exercise.bookdepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
