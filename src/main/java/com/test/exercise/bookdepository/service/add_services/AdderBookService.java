89e9d2c33509a2b3a256bff8694f5ac7e17fde32
SHCHERBA92 <vony4ka666@rambler.ru>
2023-05-16 18:56:30 +0300

    небольшие правки

--- src/main/java/com/test/exercise/bookdepository/service/add_services/AdderBookService.java
+++ src/main/java/com/test/exercise/bookdepository/service/add_services/AdderBookService.java
@@ -2,12 +2,18 @@ package com.test.exercise.bookdepository.service.add_services;
 
 import com.test.exercise.bookdepository.dto.BookDTO;
 import com.test.exercise.bookdepository.exception.BookException;
+import com.test.exercise.bookdepository.model.Author;
 import com.test.exercise.bookdepository.model.Book;
+import com.test.exercise.bookdepository.model.Genre;
+import com.test.exercise.bookdepository.repository.AuthorRepository;
 import com.test.exercise.bookdepository.repository.BookRepository;
+import com.test.exercise.bookdepository.repository.GenreRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 //TODO: добавить логирование
@@ -16,10 +22,17 @@ public class AdderBookService implements AddEntity<BookDTO>{
 
 
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final GenreRepository genreRepository;
     private final ModelMapper modelMapper;
 
-    public AdderBookService(BookRepository bookRepository, ModelMapper modelMapper) {
+    public AdderBookService(BookRepository bookRepository,
+                            AuthorRepository authorRepository,
+                            GenreRepository genreRepository,
+                            ModelMapper modelMapper) {
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.genreRepository = genreRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -32,6 +45,17 @@ public class AdderBookService implements AddEntity<BookDTO>{
     public void add(BookDTO bookDTO) {
         if (bookDTO == null) throw new BookException("Книга не задана");
         Book book = modelMapper.map(bookDTO, Book.class);
+
+        String genreName = book.getGenre().getName();
+        genreRepository.findByName(genreName).ifPresent(genre -> book.setGenre(genre));
+
+        Set<Author> authors = book.getAuthors();
+        Set<Author> newAuthors = new HashSet<>();
+        for (Author author : authors) {
+            authorRepository.findAuthorByNameAndSureName(author.getName(), author.getSureName())
+                    .ifPresentOrElse(auth ->newAuthors.add(auth), () -> newAuthors.add(author));
+        }
+        book.setAuthors(newAuthors);
         bookRepository.saveAndFlush(book);
     }
 
